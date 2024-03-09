a81167ebbb607f56435b5923328fd16bd28db2cf
martin <martin.glvn.gnlz@gmail.com>
2024-03-09 02:54:17 -0600

    Unimported updates

--- mersennePrimes.cpp
+++ mersennePrimes.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void calculate(int p, long long& num)
 {
-	num = (pow(2, p) - 1) * pow(2, (p-1));	//	p = 2, 3, 5, 7, 13, 17, 19, 31, 67|, 127, 257
+	num = (pow(2, p) - 1) * pow(2, (p - 1));	//	p = 2, 3, 5, 7, 13, 17, 19, 31, 67|, 127, 257
 }
 
 int main()
