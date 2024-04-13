32b68522ad85f87a465cb84895da9aecfa6d462f
martin <martin.glvn.gnlz@gmail.com>
2024-04-12 22:51:46 -0500

    Updated Unigrams v3

--- Unigrams v3.cpp
+++ Unigrams v3.cpp
@@ -6,14 +6,19 @@
 
 using namespace std;
 
-fstream file;
-int word_counter;
-vector<string> file_contents;
-set<string> file_unique_contents;
-string filename_input, word;
-ofstream out;
 
-string* dataArray = nullptr;
+fstream file;	//
+//int word_counter;
+//int index;
+int setcount;
+vector<string> file_contents;	//
+set<string> file_unique_contents;	//
+string filename_input, word;	//
+ofstream out;	//
+
+string* dataArray = nullptr;	//-
+int* unigram_count;
+
 
 void Word_Process(string& word)
 {
@@ -44,6 +49,34 @@ void CopyToArray(set<string>& s, string*& Arr)
 	}
 }
 
+int LinearSearch(const string& unigram, string*& dataset)	//	can be modified by bringing in both data sets
+{
+	for (int i = 0; i < setcount; i++)
+	{
+		if (unigram == dataset[i])
+			return i;
+	}
+
+	return -1;
+}
+
+void Copy(string*& dataset, vector<string>& v, int*& count)
+{
+	int wordcount = v.size();
+	int index;
+
+	for (int i = 0; i < wordcount; i++)
+	{
+		index = LinearSearch(v[i], dataset);
+
+		if (index >= 0)
+			count[index]++;
+		else
+			cout << "Could not find";
+	}
+}
+
+/*
 void displayVector(vector<string>& v)
 {
 	for (int i = 0; i < v.size(); i++)
@@ -70,17 +103,41 @@ void displayArray(string*& Array)
 	}
 }
 
+void displayInt(int*& arr)
+{
+	for (int i = 0; i < setcount; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+*/
+
 int main()
 {
+	/*
+	fstream file;	//
+	//int word_counter;
+	//int index;
+	int setcount = 0;
+	vector<string> file_contents;	//
+	set<string> file_unique_contents;	//
+	string filename_input, word;	//
+	ofstream out;	//
+	string* dataArray = nullptr;	//-
+	int* unigram_count;
+
+	string* dataArray = nullptr;	//-
+	int* unigram_count;
+	*/
+
 	filename_input = "C:\\3304 Input File\\data.txt";
 
 	file.open(filename_input.c_str());
 
-	word_counter = 0;
+	//word_counter = 0;
 
 	while (file >> word)
 	{
-		word_counter++;
 		Word_Process(word);
 		file_contents.push_back(word);
 		file_unique_contents.insert(word);
@@ -88,11 +145,38 @@ int main()
 
 	CopyToArray(file_unique_contents, dataArray);
 
-	displayVector(file_contents);
-	cout << endl;
-	displaySet(file_unique_contents);
-	cout << endl;
-	displayArray(dataArray);
+	setcount = file_unique_contents.size();
+	unigram_count = new int[setcount]();
+
+	Copy(dataArray, file_contents, unigram_count);
+
+	
+	/*
+	for (int i = 0; i < word_counter; i++)
+	{
+		index = LinearSearch(file_contents[i]);
+
+		if (index >= 0)
+			unigram_count[index]++;
+		else
+			cout << "Could not find";
+	}
+	*/
+
+	cout << "Unigram Count" << endl;
+	
+	for (int i = 0; i < setcount; i++)
+	{
+		cout << dataArray[i] << " " << unigram_count[i] << endl;
+	}
+
+
+	//displayInt(unigram_count);
+	//displayVector(file_contents);
+	//cout << endl;
+	//displaySet(file_unique_contents);
+	//cout << endl;
+	//displayArray(dataArray);
 
 
 }
