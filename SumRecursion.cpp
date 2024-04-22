4410c18ae2f40dd0e6d5acdfa658dc4ca9867c16
marglvn <martin.glvn.gnlz@gmail.com>
2024-04-22 10:29:05 -0500

    SumRecursion updated

--- SumRecursion.cpp
+++ SumRecursion.cpp
@@ -1,25 +1,8 @@
-/*
-Write a function that accepts as arguments :
-1. An array of integers
-2. The number of elements in the array
-
-The function needs to recursively calculate the sum of all of the numbers in
-the array.
-
-Demonstrate that the function works in a driver program.
-_________________
-Example :
-If the contents of the array is : { 2, 4, 6, 8 } .
-Then the recursive function will return 20.
-
-20 will be outputted to the Console Screen
-*/
-
 #include <iostream>
 
 using namespace std;
 
-int function(int* arr, int size)
+int function(int*& arr, int size)
 {
 	if (size == 1)
 		return arr[0];
@@ -30,14 +13,20 @@ int function(int* arr, int size)
 int main()
 {
 	int elements = 4;
-	int* values = new int[elements];
+	int* arr = new int[elements];
 
 	for (int i = 1; i < elements + 1; i++)
 	{
-		values[i - 1] = i * 2;
+		arr[i - 1] = i * 2;
 	}
 
-	int num = function(values, elements);
+	for (int i = 0; i < elements; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+	int num = function(arr, elements);
 
 	cout << num << endl;
 }
---
+++ attempt.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+bool function(string& s)
+{
+	int size = s.size();
+
+
+	if ()
+
+		return function(s);
+}
+
+int main()
+{
+	string value = "abba";
+
+
+}
